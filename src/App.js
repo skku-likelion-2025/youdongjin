adb2ae7c273bee7ea3d6f3e2bb22cb0611d507be
dingcodingcoding <ylvdj7879@naver.com>
2025-05-09 08:31:17 +0900

    week10 assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword,setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event)=> setKeyword(event.target.value)
+  useEffect( ()=>{
+    console.log("i run only once");
+  },[]);
+  useEffect(()=>{
+      console.log("i run when keyword changes",keyword);
+  },[keyword]);
+  useEffect(()=>{
+      console.log("i run when counter changes",keyword);
+  },[counter]);
+  useEffect(()=>{
+      console.log("i run when keyword&counter changes",keyword);
+  },[keyword,counter]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
-
-export default App;
+function Hello(){
+  useEffect(()=>{
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  },[]);
+  return <h1>Hello</h1>
+}
+function Hello2(){
+  useEffect(function(){
+    console.log("hi :)");
+    return function(){
+      console.log("bye :(");
+    };
+  },[]);
+}
+function App2(){
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev)=>!prev);
+  return(
+    <div>
+      {showing? <Hello2 />: null}
+      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
+    </div>
+  )
+}
+export default App2;
